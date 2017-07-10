ee2a596ebdcc286861d1bb49e9d9632bf39d1196
Emil Lindblom <emillind@kth.se>
2017-07-10 15:24:39 +0200

    Controller now takes input variables and creates board #1 Start of having splash screen create game.

--- controller.js
+++ controller.js
@@ -1,15 +1,72 @@
-let game = new beerpong(10, 2)
+let noOfCups = 10;
+let noOfBalls = 2;
+
+let game
 
 var statusMessage = document.querySelector('#statusMessage')
 
-let teamOneName = document.querySelector('.teamOne h2')
-let teamTwoName = document.querySelector('.teamTwo h2')
+let teamOne = document.querySelector('.teamOne')
+let teamTwo = document.querySelector('.teamTwo')
 
-let teamOneCups = document.querySelectorAll('.teamOne .cup')
-let teamTwoCups = document.querySelectorAll('.teamTwo .cup')
+let teamOneCups
+let teamTwoCups
+
+let teamOneName = teamOne.querySelector('h2')
+let teamTwoName = teamTwo.querySelector('h2')
 
 var bounceToggler = document.querySelector('#bounceToggler')
 var missButton = document.querySelector('#missButton')
+
+function start() {
+	let base
+	if (noOfCups === 10) base = 4
+	if (noOfCups === 6) base = 3
+	if (noOfCups === 3) base = 2
+	let indexOne = noOfCups-1
+	let indexTwo = 0
+	for (let i = 0; i < base; i++) {
+		indexOne = addRow(base-i, teamOne, indexOne)
+		indexTwo = addRow(i+1, teamTwo, indexTwo)
+	}
+
+	teamOneCups = teamOne.querySelectorAll('.cup')
+	teamTwoCups = teamTwo.querySelectorAll('.cup')
+
+	teamOneCups.forEach(cup => cup.addEventListener('click', () => {
+		game.hitCup(cup.dataset.index, -1)
+		updateUI()
+	}))
+
+	teamTwoCups.forEach(cup => cup.addEventListener('click', () => {
+		game.hitCup(cup.dataset.index, 1)
+		updateUI()
+	}))
+
+	game = new beerpong(noOfCups, noOfBalls)
+	updateUI()
+}
+
+function addRow(cups, team, startIndex) {
+	let row = document.createElement('div')
+	row.classList.add('row')
+	let nextIndex = addCups(cups, row, startIndex, team.dataset.teamid)
+	team.appendChild(row)
+	return nextIndex
+}
+
+function addCups(cups, row, index, teamID) {
+	for (var i = 0; i < cups; i++) {
+		let div = document.createElement('div')
+		div.classList.add('squareRatio')
+		let cup = document.createElement('button')
+		cup.classList.add('cup')
+		cup.dataset.index = index
+		index -= teamID
+		div.appendChild(cup)
+		row.appendChild(div)
+	}
+	return index
+}
 
 function updateUI() {
 	teamOneCups.forEach(cup => {
@@ -46,16 +103,6 @@ function updateCup(cup, status) {
 	}
 }
 
-teamOneCups.forEach(cup => cup.addEventListener('click', () => {
-	game.hitCup(cup.dataset.index, -1)
-	updateUI()
-}))
-
-teamTwoCups.forEach(cup => cup.addEventListener('click', () => {
-	game.hitCup(cup.dataset.index, 1)
-	updateUI()
-}))
-
 missButton.addEventListener('click', () => {
 	game.miss()
 	updateUI()
@@ -65,4 +112,4 @@ bounceToggler.addEventListener('change', () => {
 	game.bounceActive = bounceToggler.checked
 })
 
-document.addEventListener('DOMContentLoaded', updateUI)
+document.addEventListener('DOMContentLoaded', start)
